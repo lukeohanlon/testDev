3482a84ff2bc82d37aad5827bc06522f7465e3e6
luke0han <lukeohanlon@outlook.com>
2023-07-09 15:40:07 +0100

    test modules

--- modules/utils.js
+++ modules/utils.js
@@ -5,10 +5,10 @@ function subtract(num1, num2) {
     return num1-num2
 }
 function multiply(num1, num2) {
-    return num1+num2
+    return num1*num2
 }
 function divide(num1, num2) {
-    return num1%num2
+    return num1/num2
 }
 
 module.exports = {add, subtract, multiply, divide}
